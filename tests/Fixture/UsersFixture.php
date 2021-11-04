bc83ca4a5d2142e5795ecea1e8da85c23958917d
Andrés <c18050438@monclova.tecnm.mx>
2021-11-03 19:01:44 -0600

    Hasta aquí se hizo el bake, con todo y role

---
+++ tests/Fixture/UsersFixture.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * UsersFixture
+ */
+class UsersFixture extends TestFixture
+{
+    /**
+     * Init method
+     *
+     * @return void
+     */
+    public function init(): void
+    {
+        $this->records = [
+            [
+                'id' => 1,
+                'email' => 'Lorem ipsum dolor sit amet',
+                'password' => 'Lorem ipsum dolor sit amet',
+                'role' => 'Lorem ipsum dolor ',
+                'created' => '2021-11-04 00:53:10',
+                'modified' => '2021-11-04 00:53:10',
+            ],
+        ];
+        parent::init();
+    }
+}
